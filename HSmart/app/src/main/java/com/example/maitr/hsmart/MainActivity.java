ed8d2fb799bb17598621c8894f226ad7f599e553
Mai Vo <mvo36@gatech.edu>
2019-10-26 22:51:29 -0400

    Created pages and linked them to buttons

--- HSmart/app/src/main/java/com/example/maitr/hsmart/MainActivity.java
+++ HSmart/app/src/main/java/com/example/maitr/hsmart/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.maitr.hsmart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
@@ -13,77 +20,47 @@ import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
-    private MapView mMapView;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
+    /**
+     * Creates the activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button favButton = findViewById(R.id.favButton);
+        favButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View _view) {
+                Intent i = new Intent(getApplicationContext(), FavoritesActivity.class);
+                startActivity(i);
+            }
+        });
 
-        mMapView = findViewById(R.id.mapView);
-        setupMap();
+        Button cuisineButton = findViewById(R.id.cuisineButton);
+        cuisineButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View _view) {
+                Intent i = new Intent(getApplicationContext(), CuisinesActivity.class);
+                startActivity(i);
+            }
+        });
 
-//        mTextMessage = (TextView) findViewById(R.id.message);
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Button to get to dishButton
+        Button dishButton = findViewById(R.id.dishButton);
+        dishButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View _view) {
+                    Intent i = new Intent(getApplicationContext(), mapArcGISActivity.class);
+                    startActivity(i);
+                }
+        });
+
     }
 
-    private void setupMap() {
-        if (mMapView != null) {
-            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
-            double latitude = 34.09042;
-            double longitude = -118.71511;
-            int levelOfDetail = 11;
-            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
-            mMapView.setMap(map);
-        }
-    }
 
-    @Override
-    protected void onPause() {
-        if (mMapView != null) {
-            mMapView.pause();
-        }
-        super.onPause();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mMapView != null) {
-            mMapView.resume();
-        }
-    }
 
-    @Override
-    protected void onDestroy() {
-        if (mMapView != null) {
-            mMapView.dispose();
-        }
-        super.onDestroy();
-    }
+
 
 }
---
+++ HSmart/app/src/main/java/com/example/maitr/hsmart/mapArcGISActivity.java
@@ -0,0 +1,61 @@
+package com.example.maitr.hsmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class mapArcGISActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_arc_gis);
+
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+    }
+
+
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 34.09042;
+            double longitude = -118.71511;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+}
